e4fc8b37aafd10b6d322ebbf8e2020b0fe0dd071
Jessica Do <jessicanhu.do@gmail.com>
2025-03-24 23:35:16 -0500

    fix about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 // import coverPhoto from "/src/assets/images/coverphoto.jpg";
-import background from "/src/assets/images/background.jpg";
+import nature from "/src/assets/images/nature.jpg";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -8,26 +8,28 @@ gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
   useEffect(() => {
-    // gsap.fromTo(
-    //   ".ABOUT-TITLE",
-    //   { opacity: 0, y: -100 },
-    //   {
-    //     opacity: 1,
-    //     y: 0,
-    //     duration: 2.5,
-    //     ease: "power2.out",
-    //     scrollTrigger: {
-    //       trigger: "#About",
-    //       start: "top 80%",
-    //       end: "top 50%",
-    //       once: true,
-    //     },
-    //   }
-    // );
-
-    // Background transition for entire page
     ScrollTrigger.create({
       trigger: "#About",
+      start: "top 60%",
+      end: "bottom 60%",
+      onEnter: () => {
+        gsap.to(".leftcontainer", {
+          display: "block",
+          duration: 0.2,
+          ease: "power1.inOut",
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to(".leftcontainer", {
+          display: "none",
+          duration: 0.2,
+          ease: "power1.inOut",
+        });
+      },
+    });
+    // Background transition for entire page
+    ScrollTrigger.create({
+      trigger: "#Projects",
       start: "top 60%",
       end: "bottom 60%",
       onEnter: () => {
@@ -74,10 +76,10 @@ const About = () => {
   return (
     <section
       id="About"
-      className="About flex items-center h-[150vh] w-full relative" // Adjust height to 100vh for viewport fitting
+      className="About flex items-center h-[100vh] w-full relative" // Adjust height to 100vh for viewport fitting
     >
-      <div className="leftcontainer relative top-0 left-0">
-        <img className="h-[150vh]" src={background}></img>
+      <div className="leftcontainer relative h-1/10 top-0 left-0">
+        <img className=" items-center flex" src={nature}></img>
       </div>
 
       <div className="rightcontainer" id="rightcontainer"></div>
