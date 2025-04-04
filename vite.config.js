b8c881d0fefbde7177cec4dc8cc51ea739477515
Jessica Do <jessicanhu.do@gmail.com>
2025-04-04 00:08:34 -0500

    4.4 final commit domain

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/jessiixynhu/", // Replace with your repo name
+  base: "/", // Replace with your repo name
 });
